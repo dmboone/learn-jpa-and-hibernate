035f5191aaa4cc1bce2bb41eb806458112bf4551
Destiny Boone <destaj.db@gmail.com>
2023-02-28 13:16:55 -0500

    launching up h2 console FIX

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,5 +1,8 @@
-create table course(
-    id bigint not null,
-    name varchar(255) not null,
-    author varchar(255) not null,
+create table course
+(
+	id bigint not null,
+	name varchar(255) not null,
+	author varchar(255) not null,
+	primary key (id)
+	
 );
