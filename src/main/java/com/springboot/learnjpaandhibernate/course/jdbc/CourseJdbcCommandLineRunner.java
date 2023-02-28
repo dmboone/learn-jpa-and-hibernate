9026125ab12b3ceeef62def8d22a996a7e32a4a5
Destiny Boone <destaj.db@gmail.com>
2023-02-28 14:15:59 -0500

    inserting hardcoded data using spring jdbc

---
+++ src/main/java/com/springboot/learnjpaandhibernate/course/jdbc/CourseJdbcCommandLineRunner.java
@@ -0,0 +1,17 @@
+package com.springboot.learnjpaandhibernate.course.jdbc;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CourseJdbcCommandLineRunner implements CommandLineRunner { //indicates that a bean should run when it is contained within a SpringApplication
+	
+	@Autowired
+	private CourseJdbcRepository repository;
+	
+	@Override
+	public void run(String... args) throws Exception{
+		repository.insert();
+	}
+}
